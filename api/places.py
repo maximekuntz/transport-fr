6433579f4254003f915c7c7585e9e66229bc9d55
Maxime Kuntz <maxime.kuntz75@gmail.com>
2024-07-12 12:24:35 +0200

    Get places
            return self.name
    
    class PlacesAPI(TransportAPI):
        endpoint = TransportAPI.endpoint + "/api/places"
    
        def query(self, q: str) -> list[Place]:
            params = {"q": q}
            response = self._process_request(endpoint=self.endpoint, params=params)
            places = [Place(**place) for place in response]
            return places

---
+++ api/places.py
@@ -0,0 +1,12 @@
+from api.transport import TransportAPI
+from objects.places import *
+
+
+class PlacesAPI(TransportAPI):
+    endpoint = TransportAPI.endpoint + "/api/places"
+
+    def query(self, q: str) -> list[Place]:
+        params = {"q": q}
+        response = self._process_request(endpoint=self.endpoint, params=params)
+        places = [Place(**place) for place in response]
+        return places
